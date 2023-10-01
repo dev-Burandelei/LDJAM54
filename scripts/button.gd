87f2702b3d71005587ffb46f1bd48cf168b8e112
Burandelei <92280548+dev-Burandelei@users.noreply.github.com>
2023-10-01 17:45:19 -0300

    button
    Bugado com erro de colisão

---
+++ scripts/button.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+
+signal held_down
+signal release
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
+
+
+func _on_body_entered(body):
+	if body.name == "Cleito":
+		emit_signal("held_down")
+		print("held down")
+		
+		
+func _on_body_exited(body):
+	if body.name == "Cleito":
+		emit_signal("release")
+		print("release")
