9680920e4b979f1ab7b5f8daa5550b89ff0db60c
Burandelei <92280548+dev-Burandelei@users.noreply.github.com>
2023-10-02 17:26:46 -0300

    botão arrumado

--- scripts/button.gd
+++ scripts/button.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-
+var can_release = true 
 signal held_down
 signal release
 # Called when the node enters the scene tree for the first time.
@@ -13,13 +13,27 @@ func _process(_delta):
 	pass
 
 
-func _on_body_entered(body):
-	if body.name == "Cleito":
-		emit_signal("held_down")
-		print("held down")
-		
-		
-func _on_body_exited(body):
-	if body.name == "Cleito":
-		emit_signal("release")
-		print("release")
+#func _on_body_entered(body):
+#	if body.name == "Cleito":
+#		emit_signal("held_down")
+#		print("held down")
+#		can_release = false
+#		$Timer.start()
+#
+#func _on_body_exited(body):
+#	if body.name == "Cleito" and can_release:
+#
+#		emit_signal("release")
+#		print("release")
+
+
+func _on_area_entered(area):
+	emit_signal("held_down")
+	print("held down")
+
+
+
+
+func _on_area_exited(area):
+	emit_signal("release")
+	print("release")
--- testmap_vand.gd
+++ testmap_vand.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var emitter_node
 var num_buttons = 0
-var completed_value = 2
+var completed_value = 1
 var expansion = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,7 +18,7 @@ func _connect_all_buttons():
 		button_node.connect("release", Callable(self, "_on_release_signal_received"))
 		
 func _on_held_down_signal_received():
-	num_buttons += 2
+	num_buttons += 1
 	if num_buttons == completed_value:
 		call_expansion()
 	
@@ -36,7 +36,10 @@ func call_expansion():
 			call_expansion_3()
 		4:
 			call_expansion_4()
-
+		5:
+			call_expansion_5()
+	print(num_buttons)
+	print(completed_value)
 func call_expansion_1():
 	$Camera2D.zoom = Vector2(9, 5)
 	disebled_node($Node_1/button_1)
@@ -50,6 +53,12 @@ func disebled_node(node):
 	node.set_deferred("monitotable", false)
 	node.set_process(false)
 	
+func activate_node(node):
+	node.show()
+	node.set_deferred("monitoring", true)
+	node.set_deferred("monitotable", true)
+	node.set_process(true)
+	
 func disebled_mist(node):
 	node.visible = false
 	#$mist_1.collision_layer = 0
@@ -58,21 +67,31 @@ func call_expansion_2():
 	$Camera2D.zoom = Vector2(7, 4)
 	disebled_node($Node_2/button_2)
 	disebled_mist($Node_2/mist_2)
-	completed_value = 2
-	num_buttons = 1
+	completed_value = 1
+	num_buttons = 0
 
 func call_expansion_3():
 	$Camera2D.zoom = Vector2(6, 3)
 	disebled_node($Node_3/button_3)
 	disebled_mist($Node_3/mist_3)
-	completed_value = 3
-	num_buttons = 1
+	completed_value = 1
+	num_buttons = 0
 
 func call_expansion_4():
-	$Camera2D.zoom = Vector2(6, 3)
+	$Camera2D.zoom = Vector2(5, 3)
 	disebled_node($Node_4/button_4)
 	disebled_node($Node_4/button_4_2)
 	disebled_mist($Node_4/mist_4)
-	completed_value = 1
-	num_buttons = 1
+	activate_node($Node_5/button_5_1)
+	activate_node($Node_5/button_5_2)
+	completed_value = 2
+	num_buttons = 0
+	
+func call_expansion_5():
+	$Camera2D.zoom = Vector2(4, 3)
+	disebled_node($Node_5/button_5_1)
+	disebled_node($Node_5/button_5_2)
+	disebled_mist($Node_5/mist_5)
+	completed_value = 2
+	num_buttons = 0
 	
